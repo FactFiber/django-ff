35d0ab9b7f3a4fed07c320567806f52ca5432f0d
Kamil Grymuza <kamil@grymuza.com>
2011-11-17 02:11:12 +0100

    Fixed the kwargs bug in BaseListView and BaseDetailView

--- django/views/generic/list.py
+++ django/views/generic/list.py
@@ -118,7 +118,7 @@ class BaseListView(MultipleObjectMixin, View):
         if not allow_empty and len(self.object_list) == 0:
             raise Http404(_(u"Empty list and '%(class_name)s.allow_empty' is False.")
                           % {'class_name': self.__class__.__name__})
-        context = self.get_context_data(object_list=self.object_list)
+        context = self.get_context_data(object_list=self.object_list, **kwargs)
         return self.render_to_response(context)
 
 
